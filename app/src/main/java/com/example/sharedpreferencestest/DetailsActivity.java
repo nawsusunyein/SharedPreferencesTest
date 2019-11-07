d9279983205e0b1be9c2d3f39fa25eb11d726104
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-07 15:52:08 +0630

    insert shared preferences

---
+++ app/src/main/java/com/example/sharedpreferencestest/DetailsActivity.java
@@ -0,0 +1,40 @@
+package com.example.sharedpreferencestest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    TextView txtUserLoggedInName;
+    SharedPreferences shrPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        shrPref = getSharedPreferences("user_details",MODE_PRIVATE);
+        setUserLoggedInCredentials();
+    }
+
+    private void setUserLoggedInCredentials(){
+
+        txtUserLoggedInName = (TextView) findViewById(R.id.txtUserLoggedInName);
+        String username = shrPref.getString("username",null);
+        txtUserLoggedInName.setText("Hello " + username + "!");
+
+    }
+
+    public void logOut(View view){
+        Intent loginIntent = new Intent(DetailsActivity.this,MainActivity.class);
+        SharedPreferences.Editor editor = shrPref.edit();
+        editor.clear();
+        editor.commit();
+        startActivity(loginIntent);
+    }
+}
--- app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.sharedpreferencestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtUsername;
+    TextView txtPassword;
+    Button btnLogin;
+    SharedPreferences shPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtUsername = (TextView) findViewById(R.id.txtUsername);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
+    }
+
+    public void saveLoginCredentialsAndGoDetailsScreen(View view){
+        Intent detailsIntent = new Intent(MainActivity.this,DetailsActivity.class);
+        shPref = getSharedPreferences("user_details",MODE_PRIVATE);
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+
+        if(username.equals("shuoling") && password.equals("shuoling111")){
+            SharedPreferences.Editor editor = shPref.edit();
+            editor.putString("username",username);
+            editor.putString("password",password);
+            editor.commit();
+            Toast.makeText(this,"Login successful",Toast.LENGTH_LONG).show();
+            startActivity(detailsIntent);
+        }else{
+            Toast.makeText(this,"Login credentials invalid",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
